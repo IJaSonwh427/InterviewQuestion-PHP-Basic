aa16f5b04c9b8183e909abe49dd40b8168ccb3c7
IJaSonwh427 <126323612+IJaSonwh427@users.noreply.github.com>
2023-08-22 20:08:32 +0800

    Update html.php

---
+++ html.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Verify Username</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="verify_ajax.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Username Verification</h1>
+        <input type="text" id="username" placeholder="Enter your username">
+        <button id="submitBtn">Submit</button>
+        <div id="message"></div>
+    </div>
+</body>
+</html>
